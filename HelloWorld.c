3057b39dae96e9f944a7bbadcf133ae0db4e6e6b
krish420 <42693382+krish420@users.noreply.github.com>
2020-04-14 09:47:11 +0530

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -7,8 +7,9 @@ int main()
 {
 	
     char s[100];
-    scanf("%[^\n]%*c", s);
-    printf("Hello, World!\n%s",s);
-  	    
+    scanf("%[^\n]%*c", &s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    printf("Hello, World!\n%s",s);   
     return 0;
 }
